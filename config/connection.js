74cef3dbd59752f2088b909ac961774fd6aea8c7
Max Power <dbroncosfan@gmail.com>
2020-01-25 23:04:01 -0700

    multiple file updates, partially working code

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+//Set up MySql connection
+require("dotenv").config();
+var mysql = require("mysql");
+
+var data = process.env;
+
+var connection = mysql.createConnection({
+    host: data.host,
+    port: data.port,
+    user: data.user,
+    password: data.password,
+    database: data.database
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+    ("Bacon Cheese Burger", false),
+    ("Sunnyside Burger", false),
+    ("Yucatan Burger", false),
+    ("Iconic Burger", false)
